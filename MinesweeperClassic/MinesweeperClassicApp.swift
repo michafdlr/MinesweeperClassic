c4fef35192cfa0039c89b1a4702814d51a51a610
Michael Fiedler <michael.fiedler1604@gmail.com>
2025-05-25 18:23:49 +0200

    Initial Commit

---
+++ MinesweeperClassic/MinesweeperClassicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperClassicApp.swift
+//  MinesweeperClassic
+//
+//  Created by Michael Fiedler on 25.05.25.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperClassicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
